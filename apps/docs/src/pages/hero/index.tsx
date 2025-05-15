33864e345e56651ac70b0a4269a1e25785b20fc4
Oleksandr Hanzha <alex@granted.name>
2025-05-15 18:31:02 +0300

    feat: initial docs part

---
+++ apps/docs/src/pages/hero/index.tsx
@@ -0,0 +1,15 @@
+import HeroFuturistic from '@/components/blocks/hero-futuristic.tsx'
+import { HeroGeometric } from '@/components/ui/shape-landing-hero.tsx'
+import { HeroSection } from '@/components/blocks/hero-section-1.tsx'
+import HeroSectionNexus from '@/components/blocks/hero-section-nexus.tsx'
+
+export function Hero() {
+  return <HeroSectionNexus />
+  // return (
+  //   <HeroGeometric
+  //     badge="Navios"
+  //     title1="Hello, Navios"
+  //     title2="Let's build the future together"
+  //   />
+  // )
+}
